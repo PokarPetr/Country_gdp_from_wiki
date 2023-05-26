300afeebd1c00ab249a197d90517931aea600085
PokarPetr <pokar.petr@gmail.com>
2023-05-26 21:12:22 +0200

    Modified items.py. Imported TakeFirst and MapCompose. Modified Fields in CountriesGdpItem class

--- countries_gdp/countries_gdp/items.py
+++ countries_gdp/countries_gdp/items.py
@@ -1,11 +1,24 @@
-
 import scrapy
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
 
 class CountriesGdpItem(scrapy.Item):
-    country_name = scrapy.Field()
-    region = scrapy.Field()
-    gdp = scrapy.Field()
-    year = scrapy.Field()
+    country_name = scrapy.Field(
+        input_processor=MapCompose(remove_tags, str.strip),
+        output_processor=TakeFirst()
+    )
+    region = scrapy.Field(
+        input_processor=MapCompose(remove_tags, str.strip),
+        output_processor=TakeFirst()
+    )
+    gdp = scrapy.Field(
+        input_processor=MapCompose(remove_tags, str.strip),
+        output_processor=TakeFirst()
+    )
+    year = scrapy.Field(
+        input_processor=MapCompose(remove_tags, str.strip),
+        output_processor=TakeFirst()
+    )
 
 
