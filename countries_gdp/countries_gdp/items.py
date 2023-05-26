ef564c6823144af198d1bc88177f69c580e2b09c
PokarPetr <pokar.petr@gmail.com>
2023-05-26 21:50:02 +0200

    Modified items.py. Added 4 functions(try_int, try_float, _extract_year, remove_commas

--- countries_gdp/countries_gdp/items.py
+++ countries_gdp/countries_gdp/items.py
@@ -1,6 +1,29 @@
+import re
+
 import scrapy
 from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
+
+def remove_commas(value):
+    return value.replace(',', '')
+
+def try_float(value):
+    try:
+        return float(value)
+    except ValueError:
+        return value
+
+def try_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+def extract_year(value):
+    year = re.findall(r'\d{4}', value)
+    if not year:
+        return value
+    return year
 
 
 class CountriesGdpItem(scrapy.Item):
@@ -13,11 +36,11 @@ class CountriesGdpItem(scrapy.Item):
         output_processor=TakeFirst()
     )
     gdp = scrapy.Field(
-        input_processor=MapCompose(remove_tags, str.strip),
+        input_processor=MapCompose(remove_tags, str.strip, remove_commas, try_float),
         output_processor=TakeFirst()
     )
     year = scrapy.Field(
-        input_processor=MapCompose(remove_tags, str.strip),
+        input_processor=MapCompose(remove_tags, str.strip, extract_year, try_int),
         output_processor=TakeFirst()
     )
 
