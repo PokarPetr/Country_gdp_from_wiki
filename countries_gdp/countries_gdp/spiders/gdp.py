6b9edf98f0caace2f75b37f19bc4ad3c76a0306c
PokarPetr <pokar.petr@gmail.com>
2023-05-26 20:42:58 +0200

    Modified gdp spider. Added import CountriesGdpItem. Rewrote yield through item Fields

--- countries_gdp/countries_gdp/spiders/gdp.py
+++ countries_gdp/countries_gdp/spiders/gdp.py
@@ -1,4 +1,5 @@
 import scrapy
+from countries_gdp.items import CountriesGdpItem
 
 
 class GdpSpider(scrapy.Spider):
@@ -8,11 +9,13 @@ class GdpSpider(scrapy.Spider):
 
     def parse(self, response):
         for country in response.css('table.wikitable.sortable tbody tr:not([class])'):
-            yield {
-                'country_name': country.css('td:nth-child(1) a::text').get(),
-                'region': country.css('td:nth-child(2) a::text').get(),
-                'gdp': country.css('td:nth-child(3)::text').get(),
-                'year': country.css('td:nth-child(4)::text').get(),
-            }
+            item = CountriesGdpItem()
+
+            item['country_name'] =  country.css('td:nth-child(1) a::text').get()
+            item['region'] = country.css('td:nth-child(2) a::text').get()
+            item['gdp'] = country.css('td:nth-child(3)::text').get()
+            item['year'] = country.css('td:nth-child(4)::text').get()
+
+            yield item
 
 
