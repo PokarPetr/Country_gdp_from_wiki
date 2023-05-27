8dd16e55d2fd9fb744dda833bbb78ef758008085
PokarPetr <pokar.petr@gmail.com>
2023-05-27 07:18:12 +0200

    Modified pipeline.py. Created SaveToDatabasePipeline. Created open_spider method

--- countries_gdp/countries_gdp/pipelines.py
+++ countries_gdp/countries_gdp/pipelines.py
@@ -1,3 +1,4 @@
+import sqlite3
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 
@@ -9,3 +10,19 @@ class CountriesGdpPipeline:
 
         return item
 
+class SaveToDatabasePipeline:
+    def __init__(self):
+        self.con = sqlite3.connect("countries_gdp.db")
+        self.cur = self.con.cursor()
+
+    def open_spider(self, spider):
+        self.cur.execute("""CREATE TABLE IF NOT EXIST countries_gdp(
+        countries_name TEXT PRIMARY KEY, 
+        region TEXT, 
+        gdp REAL, 
+        year INTEGER)
+        """)
+        self.con.commit()
+
+    def process_item(self, item, spider):
+        pass
